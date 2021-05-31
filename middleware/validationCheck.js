6a0127a880f46b0563a4886e3057dcd9c2d9d782
Saugata <saugata97aeroxt@gmail.com>
2021-05-31 16:51:12 +0530

    question3 part 1 jwt

--- middleware/validationCheck.js
+++ middleware/validationCheck.js
@@ -10,25 +10,8 @@ module.exports = (req, res, next) => {
       message: "No token has been given, authorization denied",
     });
   try {
-<<<<<<< HEAD
     const decoded_token = jwt.verify(token, process.env.mySecretKey);
     req.user_id = decoded_token.user;
-=======
-    const valid_token = await AccessToken.findOne({ access_token });
-    if (!valid_token)
-      return res.status(400).json({
-        success: 0,
-        message: "Provided token is not valid, please provide a valid token",
-      });
-    if (new Date() > valid_token.expiry) {
-      await AccessToken.findByIdAndRemove(valid_token._id);
-      return res.status(400).json({
-        success: 0,
-        message: "Session has expired,please login again",
-      });
-    }
-    req.user_id = valid_token.user_id;
->>>>>>> 0fa7de15045e2b35550e4c4026e1e1d8268fa179
     next();
   } catch (err) {
     res.status(400).json({
