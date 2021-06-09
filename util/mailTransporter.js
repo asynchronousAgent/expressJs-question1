d7798a235a0e1f0332be717881c0f1bf054be070
Saugata <saugata97aeroxt@gmail.com>
2021-06-09 14:42:49 +0530

    mail updated

--- util/mailTransporter.js
+++ util/mailTransporter.js
@@ -1,9 +1,14 @@
 const nodemailer = require("nodemailer");
 
 module.exports = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.host_email,
     pass: process.env.host_password,
+  },
+  tls: {
+    rejectUnauthorized: false,
   },
 });
